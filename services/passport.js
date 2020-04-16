51366adbe06a43015d160a9859985487d8e47bcd
Dima <dima23790@gmail.com>
2020-04-16 17:11:31 +0300

    Added Google OAuth

---
+++ services/passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        //Check if user exists
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          //Create user
+          const user = await new User({
+            googleId: profile.id,
+          });
+          await user.save();
+
+          done(null, user);
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    }
+  )
+);
