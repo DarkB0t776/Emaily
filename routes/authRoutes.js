372ac1ea00a250bd56143d02d6d80d9a49bd8015
Dima <dima23790@gmail.com>
2020-04-16 17:26:54 +0300

    Added cookies and implemented user logout process

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,4 +12,12 @@ router.get(
 
 router.get('/auth/google/callback', passport.authenticate('google'));
 
+router.get('/api/logout', (req, res) => {
+  req.logout();
+  res.send(req.user);
+});
+
+router.get('/api/current_user', (req, res) => {
+  res.send(req.user);
+});
 module.exports = router;
